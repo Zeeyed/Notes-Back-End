39a4c52984deb34d9a5c767d9dc6ffe30ffa4d1a
Zied Ghalleb <zied.ghalleb@gmail.com>
2017-03-24 21:34:13 +0100

    Error Handling Middleware

--- index.js
+++ index.js
@@ -17,6 +17,13 @@ app.use(bodyParser.json());
  */
 app.use("/api", routes);
 
+/**
+ * Error handling middleware
+ */
+app.use(function(error, req, res, next){
+    res.status(422).send({error: error.message});
+});
+
 // Listen for requests
 app.listen(process.env.port || 4000, function(){
     console.log("Listening for requests ...");
--- routes/api.js
+++ routes/api.js
@@ -4,33 +4,33 @@ const Note = require('../models/note');
 /**
  * GET list of notes
  */
-router.get("/notes", function(req, res){
+router.get("/notes", function(req, res, next){
     res.send({type: "GET"});
 })
 
 /**
  * ADD a new note to the database 
  */
-router.post("/notes", function(req, res){
+router.post("/notes", function(req, res, next){
     //var note = new Note(req.body);
     //note.save();
     // create new instance and save it to the database
     Note.create(req.body).then(function(note){
         res.send(note);
-    });
+    }).catch(next);
 })
 
 /**
  * UPDATE a specific note
  */
-router.put("/notes/:id", function(req, res){
+router.put("/notes/:id", function(req, res, next){
     res.send({type: "PUT"});
 });
 
 /**
  * DELETE a note from the database
  */
-router.delete("/notes/:id", function(req, res){
+router.delete("/notes/:id", function(req, res, next){
     res.send({type: "DELETE"});
 });
 
